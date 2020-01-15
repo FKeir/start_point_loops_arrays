d9d6ce21f3ed53753688beee7848a9c9201fc72c
mholwill <m_holwill@hotmail.co.uk>
2020-01-15 16:02:09 +0000

    Initial commit

---
+++ my_functions.rb
@@ -0,0 +1,37 @@
+def add_array_lengths(array1, array2)
+    totallength = array1.length + array2.length
+    return totallength
+end
+
+
+
+
+def sum_array(numbers_array)
+  total = 0
+  for number in numbers_array
+    total += number
+  end
+  return total
+end
+
+
+
+
+def is_item_in_array(array, name)
+
+  for item in array
+    if item == name
+      return true
+    end
+  end
+
+  return false
+
+end
+
+
+
+
+def get_first_key(hash)
+  return hash.keys[0]
+end
